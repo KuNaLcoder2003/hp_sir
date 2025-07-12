6f17b15211b5782495cbfdb027faadacd4a25b72
Kunal Singh <kunalindia59@gmail.com>
2025-07-12 11:32:09 +0530

    import issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import Signin from './components/Signin'
-import StudentNavbar from './components/StudentNavbar'
 import StudentDashboard from './components/StudentDashboard'
 import Tests from './components/Tests'
 
