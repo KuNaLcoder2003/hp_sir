8d8b42a10bcb4eb03bcb55d6eff82e95a8fcbb8a
Kunal Singh <kunalindia59@gmail.com>
2025-08-18 18:13:37 +0530

    adding animation based components

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -6,6 +6,7 @@ import Services from '../components/Services'
 import Footer from '../components/Footer'
 import TopperList from '../components/TopperList'
 import Teacher from '../components/Teacher'
+import FlipLink from '../components/FlipLink'
 
 const LandingPage = () => {
   return (
@@ -22,7 +23,19 @@ const LandingPage = () => {
 
         <img src='/Toppers_class_10_science.jpeg' alt='topper photo' className='w-full h-full object-cover rounded-lg' />
       </div>
+
       <Teacher />
+      <div className='flex flex-col items-center mt-10 gap-2'>
+        <div className='w-[80%] m-auto flex justify-center items-center'>
+          <FlipLink href="#">Learn</FlipLink>
+        </div>
+        <div className='w-[80%] m-auto flex justify-center items-center'>
+          <FlipLink href="#">Grow</FlipLink>
+        </div>
+        <div className='w-[80%] m-auto flex justify-center items-center'>
+          <FlipLink href="#">Succeed</FlipLink>
+        </div>
+      </div>
       <div className='w-screen mt-20 flex items-center justify-center'>
         <Footer />
       </div>
