1d23521064abdd78bb710624027d3cc01f726280
Kunal Singh <kunalindia59@gmail.com>
2026-01-04 02:24:59 +0530

    Routing for chapter api routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import batchRouter from "./batch.js";
+import chapterRouter from "./chapter.js";
 
 const router = express.Router()
 
 router.use('/batch', batchRouter)
+router.use('/chapter', chapterRouter)
 
 export default router;
