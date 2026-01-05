eb228b18d13542ff2dd986a0def0ea1dc36beccc
Kunal Singh <kunalindia59@gmail.com>
2026-01-05 14:34:44 +0530

    Fixing cors issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import router from './routes/index.js';
 const app = express();
 
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 app.use('/api/v1', router)
 app.listen(3000, () => {
     console.log('App started')
