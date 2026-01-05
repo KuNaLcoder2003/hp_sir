426789c823e29de59664e1bd379cf3fbd4ed091f
Kunal Singh <kunalindia59@gmail.com>
2026-01-05 14:33:27 +0530

    New route handler for getting details of batch

--- backend/src/routes/batch.ts
+++ backend/src/routes/batch.ts
@@ -11,6 +11,7 @@ interface NewBatch {
 batchRouter.post('/', authMiddleware, async (req: express.Request, res: express.Response) => {
     try {
         const batch_details: NewBatch = req.body;
+        console.log('Reached here ')
         if (!batch_details || !batch_details.batch_description || !batch_details.batch_name) {
             res.status(400).json({
                 message: "Either Batch name or description or both missing",
@@ -28,7 +29,7 @@ batchRouter.post('/', authMiddleware, async (req: express.Request, res: express.
                 }
             })
             return { new_batch }
-        })
+        }, { timeout: 20000, maxWait: 10000 })
 
         if (!response.new_batch) {
             res.status(403).json({
@@ -39,6 +40,55 @@ batchRouter.post('/', authMiddleware, async (req: express.Request, res: express.
         res.status(200).json({
             message: 'Batch created',
             valid: true
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong",
+            valid: false,
+            error: error
+        })
+    }
+})
+
+batchRouter.get('/batchDetails/:batchId', async (req: express.Request, res: express.Response) => {
+    try {
+        const batchId = req.params.batchId;
+        if (!batchId) {
+            res.status(400).json({
+                message: "Bad request",
+                valid: false
+            })
+            return
+        }
+        const response = await prisma.$transaction(async (tx) => {
+            const batch = await tx.batch.findUnique({
+                where: {
+                    id: batchId
+                },
+                select: {
+                    batch_name: true,
+                    batch_description: true
+                }
+            })
+            const chapters = await tx.chapter.findMany({
+                where: {
+                    batch_id: batchId
+                }
+            })
+            return { batch, chapters }
+        }, { timeout: 20000, maxWait: 10000 })
+
+        if (!response || !response.batch) {
+            res.status(404).json({
+                message: "Batch not found",
+                valid: false
+            })
+            return
+        }
+        console.log(response.batch)
+        res.status(200).json({
+            batch_details: response.batch,
+            batch_chapters: response.chapters
         })
     } catch (error) {
         res.status(500).json({
