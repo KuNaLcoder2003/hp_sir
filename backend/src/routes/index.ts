ee2b31afe8a6ff5d7ea1d9f584363f919c7a2d72
Kunal Singh <kunalindia59@gmail.com>
2026-01-05 14:32:04 +0530

    Adding '/notes' and '/videos' to the router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import batchRouter from "./batch.js";
 import chapterRouter from "./chapter.js";
+import notesRouter from "./notes.js";
+import videoRouter from "./video.js";
 
 const router = express.Router()
 
 router.use('/batch', batchRouter)
 router.use('/chapter', chapterRouter)
+router.use('/notes', notesRouter)
+router.use('/video', videoRouter)
 
 export default router;
