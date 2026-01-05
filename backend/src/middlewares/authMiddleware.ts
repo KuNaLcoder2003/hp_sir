9100d86f90d86ea6623e49ecf6d0c18b1aa09b15
Kunal Singh <kunalindia59@gmail.com>
2026-01-05 14:35:44 +0530

    Auth middleware for uploading Batches , Chapters , Materials , Videos

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,24 +1,32 @@
 import express from "express"
 function authMiddleware(req: express.Request, res: express.Response, next: express.NextFunction) {
     try {
+        console.log(1);
         const code = req.headers.authorization;
         if (!code) {
+            console.log(2);
             res.status(401).json({
-                message: "You donot have permission for this action",
+                message: "You do not have permission for this action",
                 valid: false
             })
             return
         }
+
         if (code == 'DYRTN_BY_HP_SIR_6/120_ABHAYASA') {
+            console.log(3);
             next();
+
         } else {
+            console.log(4);
             res.status(401).json({
-                message: "You donot have permission for this action",
+                message: "You do not have permission for this action",
                 valid: false
             })
             return
         }
+        console.log(5);
     } catch (error) {
+        console.log(6);
         res.status(500).json({
             message: "Something went wrong",
             valid: false
