4ecfdd87a65fbc05f959b58356495083ee039994
Kunal Singh <kunalindia59@gmail.com>
2026-01-10 01:36:28 +0530

    New route for admin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import Batch from './components/Batch'
 import NotFound from './components/NotFound'
+import AdminBatches from './components/AdminBatches'
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <Route path='/' element={<LandingPage />} />
       <Route path='/batch/:batchId' element={<Batch />} />
       <Route path='/batch/err' element={<NotFound />} />
+      <Route path='/admin' element={<AdminBatches />} />
     </Routes>
   )
 }
